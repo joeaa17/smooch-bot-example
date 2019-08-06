50f16c126a67d98434f56f79fea3675232df2453
joeaa17 <32424114+joeaa17@users.noreply.github.com>
2019-08-06 17:44:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,36 +2,58 @@
 
 const Script = require('smooch-bot').Script;
 
-module.exports = new Script({
-    processing: {
-        prompt: (bot) => bot.say('Beep boop...'),
-        receive: () => 'processing'
-    },
 
+const script = new Script({
     start: {
         receive: (bot) => {
-            return bot.say('Hi! I\'m Smooch Bot!')
+            return bot.say('Welcome to your digital butler! I’m Max, your personal AI concierge. You can talk to me about anything that`s on your mind.')
                 .then(() => 'askName');
         }
     },
 
     askName: {
-        prompt: (bot) => bot.say('What\'s your name?'),
+        prompt: (bot) => bot.say('Type in your name sir/madam.'),
         receive: (bot, message) => {
-            const name = message.text;
-            return bot.setProp('name', name)
-                .then(() => bot.say(`Great! I'll call you ${name}
-Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
-                .then(() => 'finish');
+            const name = message.text.trim();
+            bot.setProp('name', name);
+            return bot.say('Great! I`ll call you ${name}' + 'Is that OK? %[Yes](postback:yes) %[No](postback:no)')
+                .then(() => 'next');
         }
     },
 
-    finish: {
+    next: {
         receive: (bot, message) => {
             return bot.getProp('name')
-                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
-                        'teach me how to do anything else!'))
-                .then(() => 'finish');
+                .then(() => 'getRequest');
+        }
+    },
+
+    getRequest: {
+        prompt: (bot) => bot.say('${name}, how can I help you have a better stay?'),
+        receive: (bot, message) => {
+            const zCurrResponse = message.text.trim();
+
+            var str = zCurrResponse + "";
+            var regex = RegExp('^(.*?(\bResto\b)[^$]*)$|^(.*?(\bRestaurant\b)[^$]*)$|^(.*?(\bFood\b)[^$]*)$|^(.*?(\bIce Cream\b)[^$]*)$|^(.*?(\bDessert\b)[^$]*)$|^(.*?(\bDesert\b)[^$]*)$|^(.*?(\bDeserts\b)[^$]*)$|^(.*?(\bSushi\b)[^$]*)$|^(.*?(\bChinese\b)[^$]*)$|^(.*?(\bJapanese\b)[^$]*)$|^(.*?(\bIndian\b)[^$]*)$|^(.*?(\bMexican\b)[^$]*)$|^(.*?(\bMediterranean\b)[^$]*)$|^(.*?(\bFast Food\b)[^$]*)$|^(.*?(\bMcDonalds\b)[^$]*)$|^(.*?(\bBurger King\b)[^$]*)$|^(.*?(\bStarbucks\b)[^$]*)$|^(.*?(\bBurger\b)[^$]*)$|^(.*?(\bPizza\b)[^$]*)$|^(.*?(\bVegan\b)[^$]*)$|^(.*?(\bVegetarian\b)[^$]*)$|^(.*?(\bFine Dining\b)[^$]*)$|^(.*?(\btake out\b)[^$]*)$|^(.*?(\brestaurants\b)[^$]*)$|^(.*?(\breserve\b)[^$]*(\btable\b)[^$]*)$');
+            //console.log(regex.test(str) + " " + regex.lastIndex);
+            // expected output: true
+
+            if(regex.test(str)){
+                return bot.say('A great time for food! I found diners near you, to check out, please follow the link below: \n https://www.google.com/search?q=restaurants+near+me')
+                .then(() => 'next');
+            }
+            else {
+                return bot.say('Please be precise and concise to guide my understanding. what are you looking for?')
+                .then(() => 'next');          
+            }
+
+
+
+          //  bot.setProp('name', name);
+          //  return bot.say('I`ll call you ${name}! Great!')
+          //     .then(() => 'finish');
         }
     }
+
+
 });
